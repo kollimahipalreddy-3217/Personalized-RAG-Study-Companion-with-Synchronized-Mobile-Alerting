e01b800f6a123a1ab797bd57aa7deb6b55d8ac6c
kollimahipalreddy-3217 <kollimahipalreddy@gmail.com>
2026-09-03 13:59:23 +0530

    Core AI engine package initializer

---
+++ core/__init__.py
@@ -0,0 +1,7 @@
+﻿import os
+import sys
+
+_ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+for _p in [_ROOT_DIR, os.path.join(_ROOT_DIR, "core"), os.path.join(_ROOT_DIR, "services"), os.path.join(_ROOT_DIR, "config")]:
+    if _p not in sys.path:
+        sys.path.insert(0, _p)
